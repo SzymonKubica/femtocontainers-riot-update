fa75d9e7edf8026c291a4f0625b492a59ed7f35c
Szymon Kubica <szymo.kubica@gmail.com>
2024-01-13 16:57:11 +0100

    Separated the diode example from the fletcher32 benchmark logic.

--- examples/suit_femtocontainer/bpf/fletcher-32-checksum.c
+++ examples/suit_femtocontainer/bpf/fletcher-32-checksum.c
@@ -50,29 +50,6 @@ int fletcher32_bench(void *ctx)
     uint32_t checksum = (c1 << 16 | c0);
     uint32_t end = bpf_ztimer_now();
 
-    // Play around with the diodes here:
-    bpf_saul_reg_t *diode;
-    phydat_t diode_payload;
-
-    uint32_t last_wakeup = bpf_ztimer_now();
-
-    // Toggle all onboard LEDs in order
-    int diode_index = 0;
-    int count = 0;
-    int max_iterations = 100;
-    while (count++ < max_iterations) {
-        bpf_ztimer_periodic_wakeup(&last_wakeup, PERIOD_US);
-        // First turn off the current diode
-        diode = bpf_saul_reg_find_nth(diode_index);
-        diode_payload.val[0] = 0;
-        bpf_saul_reg_write(diode, &diode_payload);
-        // Now increment the diode index and turn it on
-        diode_index = (diode_index + 1) % 3;
-        diode = bpf_saul_reg_find_nth(diode_index);
-        diode_payload.val[0] = 1;
-        bpf_saul_reg_write(diode, &diode_payload);
-    }
-
     // We return the computation time in microseconds
     return end - start;
 }
